00c119a35a286485b44cc50e09da41f15957ac02
lehelen19 <le.helen19@gmail.com>
2023-06-06 11:30:39 -0700

    Add Routes to App (HomePage /)

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from '../HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+        </Routes>
+      </>
     </div>
   );
 }
