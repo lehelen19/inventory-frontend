7fc89567f523d40066516786b8953eb4507fd524
lehelen19 <le.helen19@gmail.com>
2023-06-06 11:45:17 -0700

    Delete unnecessary boilerplate

---
+++ src/utilities/users/users-service.js
@@ -0,0 +1,35 @@
+import * as userAPI from './users-api';
+
+export async function signUp(userData) {
+  const token = await userAPI.signUp(userData);
+  localStorage.setItem('token', token);
+  return getUser();
+}
+
+export function getToken() {
+  const token = localStorage.getItem('token');
+  if (!token) return null;
+
+  const payload = JSON.parse(window.atob(token.split('.')[1]));
+  if (payload.exp < Date.now() / 1000) {
+    localStorage.removeItem('token');
+    return null;
+  }
+
+  return token;
+}
+
+export function getUser() {
+  const token = getToken();
+  return token ? JSON.parse(window.atob(token.split('.')[1])).user : null;
+}
+
+export async function login(userData) {
+  const token = await userAPI.login(userData);
+  localStorage.setItem('token', token);
+  return getUser();
+}
+
+export function logOut() {
+  localStorage.removeItem('token');
+}
