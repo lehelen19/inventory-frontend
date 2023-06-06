7fa6ec2ff0b062cb266b4f37807d6ca69619a41e
lehelen19 <le.helen19@gmail.com>
2023-06-06 14:35:51 -0700

    Create fetchfooddetails fn

--- src/pages/FoodItemDetailPage/FoodItemDetailPage.js
+++ src/pages/FoodItemDetailPage/FoodItemDetailPage.js
@@ -1,4 +1,19 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
 const FoodItemDetailPage = () => {
+  const [foodDetails, setFoodDetails] = useState(null);
+  const { id } = useParams();
+
+  const fetchFoodDetails = async () => {
+    try {
+      const foundFoodDetails = await getFoodDetails(id);
+      setFoodDetails(foundFoodDetails);
+    } catch {
+      console.log('error getting food details');
+    }
+  };
+
   return <div>FoodItemDetailPage</div>;
 };
 
