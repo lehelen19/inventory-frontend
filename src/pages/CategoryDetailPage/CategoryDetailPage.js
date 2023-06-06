9c92e01f65d983d6e685588784abaac169473dfd
lehelen19 <le.helen19@gmail.com>
2023-06-06 13:21:08 -0700

    Define fetchCategoryDetails in CategoryDetailPage

--- src/pages/CategoryDetailPage/CategoryDetailPage.js
+++ src/pages/CategoryDetailPage/CategoryDetailPage.js
@@ -1,4 +1,19 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
 const CategoryDetailPage = () => {
+  const [categoryDetails, setCategoryDetails] = useState(null);
+  const { id } = useParams();
+
+  const fetchCategoryDetails = async () => {
+    try {
+      const foundCategory = await getCategoryDetails(id);
+      setCategoryDetails(foundCategory);
+    } catch {
+      console.log('ERROR');
+    }
+  };
+
   return <div>CategoryDetailPage</div>;
 };
 
