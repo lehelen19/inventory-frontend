2b324be76dd03010a167b744ce9ce93df9be2574
lehelen19 <le.helen19@gmail.com>
2023-06-06 11:33:06 -0700

    Create NavBar component

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>About</li>
+        <li>Inventory</li>
+        <li>Admin Login</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../HomePage/HomePage';
+import Navbar from '../../components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <>
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
