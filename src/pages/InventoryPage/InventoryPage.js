df2dd915d85575f0fa83a2cdb08331befb64b241
lehelen19 <le.helen19@gmail.com>
2023-06-06 11:50:08 -0700

    Create fetchCats fn on inventorypage

--- src/pages/InventoryPage/InventoryPage.js
+++ src/pages/InventoryPage/InventoryPage.js
@@ -1,4 +1,18 @@
+import { useState, useEffect } from 'react';
+import { getCategories } from '../../utilities/categories/categories-api';
+
 const InventoryPage = () => {
+  const [categories, setCategories] = useState(null);
+
+  const fetchCategories = async () => {
+    try {
+      const foundCategories = await getCategories();
+      setCategories(foundCategories);
+    } catch {
+      console.log('Error');
+    }
+  };
+
   return (
     <>
       <h1>Inventory Page</h1>
