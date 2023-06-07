05cc5dc601513a13e6049324b630065a3c0bcc4a
lehelen19 <le.helen19@gmail.com>
2023-06-07 15:30:07 -0700

    Add logout button to navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,18 @@
 import { Link } from 'react-router-dom';
+import { logOut } from '../../utilities/users/users-service';
 
-const Navbar = () => {
+const Navbar = ({ user, setUser }) => {
+  const handleLogout = () => {
+    logOut();
+    setUser(null);
+  };
+
   return (
     <nav>
       <Link to="/">Logo</Link>
       <Link to="/about">About</Link>
       <Link to="/categories">Inventory</Link>
+      <button onClick={handleLogout}>Log Out</button>
     </nav>
   );
 };
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <div className="App">
-      <Navbar />
+      <Navbar user={user} setUser={setUser} />
       <>
         {user ? (
           <Routes>
