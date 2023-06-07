20269d031d495ff1295c96fe8faf655808d9eebe
lehelen19 <le.helen19@gmail.com>
2023-06-07 15:07:39 -0700

    Add user state to App and link AuthPage

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,27 +1,36 @@
-import './App.css';
+import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { getUser } from '../../utilities/users/users-service';
 import HomePage from '../HomePage/HomePage';
+import AuthPage from '../AuthPage/AuthPage';
 import Navbar from '../../components/NavBar/NavBar';
 import InventoryPage from '../InventoryPage/InventoryPage';
 import AboutPage from '../AboutPage/AboutPage';
 import CategoryDetailPage from '../CategoryDetailPage/CategoryDetailPage';
 import FoodItemDetailPage from '../FoodItemDetailPage/FoodItemDetailPage';
+import './App.css';
 
 function App() {
+  const [user, setUser] = useState(getUser());
+
   return (
     <div className="App">
       <Navbar />
       <>
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/about" element={<AboutPage />}></Route>
-          <Route path="/categories" element={<InventoryPage />}></Route>
-          <Route
-            path="/categories/:id"
-            element={<CategoryDetailPage />}
-          ></Route>
-          <Route path="/items/:id" element={<FoodItemDetailPage />}></Route>
-        </Routes>
+        {user ? (
+          <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/about" element={<AboutPage />}></Route>
+            <Route path="/categories" element={<InventoryPage />}></Route>
+            <Route
+              path="/categories/:id"
+              element={<CategoryDetailPage />}
+            ></Route>
+            <Route path="/items/:id" element={<FoodItemDetailPage />}></Route>
+          </Routes>
+        ) : (
+          <AuthPage />
+        )}
       </>
     </div>
   );
