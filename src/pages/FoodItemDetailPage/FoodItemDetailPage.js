a1d903cf616ee1c0fa0d75ead3a898c22a1b016d
lehelen19 <le.helen19@gmail.com>
2023-06-07 15:58:09 -0700

    Add delete item button

--- src/pages/FoodItemDetailPage/FoodItemDetailPage.js
+++ src/pages/FoodItemDetailPage/FoodItemDetailPage.js
@@ -23,30 +23,33 @@ const FoodItemDetailPage = () => {
 
   const loaded = () => {
     return (
-      <div>
-        <h1>{foodDetails.name}</h1>
-        <p>Quantity: {foodDetails.quantity}</p>
+      <>
+        <div>
+          <h1>{foodDetails.name}</h1>
+          <p>Quantity: {foodDetails.quantity}</p>
 
-        <h2>Nutritional Value for {foodDetails.food_name}</h2>
-        <p>
-          Serving size: {foodDetails.serving_qty} {foodDetails.serving_unit} or{' '}
-          {foodDetails.serving_weight_grams} grams
-        </p>
-        <p>Calories: {foodDetails.nf_calories}</p>
-        <p>Total fat: {foodDetails.nf_total_fat}g</p>
-        <p>Saturated fat: {foodDetails.nf_saturated_fat}g</p>
-        <p>Cholesterol: {foodDetails.nf_cholesterol}mg</p>
-        <p>Sodium: {foodDetails.nf_sodium}mg</p>
-        <p>Total carbohydrates: {foodDetails.nf_total_carbohydrate}g</p>
-        <p>Dietary fiber: {foodDetails.nf_dietary_fiber}g</p>
-        <p>Sugars: {foodDetails.nf_sugars}g</p>
-        <p>Protein: {foodDetails.nf_protein}g</p>
-        <p>Potassium: {foodDetails.nf_potassium}mg</p>
-        <img
-          src={`${foodDetails.photo.thumb}`}
-          alt={`Thumbnail of ${foodDetails.food_name}`}
-        />
-      </div>
+          <h2>Nutritional Value for {foodDetails.food_name}</h2>
+          <p>
+            Serving size: {foodDetails.serving_qty} {foodDetails.serving_unit}{' '}
+            or {foodDetails.serving_weight_grams} grams
+          </p>
+          <p>Calories: {foodDetails.nf_calories}</p>
+          <p>Total fat: {foodDetails.nf_total_fat}g</p>
+          <p>Saturated fat: {foodDetails.nf_saturated_fat}g</p>
+          <p>Cholesterol: {foodDetails.nf_cholesterol}mg</p>
+          <p>Sodium: {foodDetails.nf_sodium}mg</p>
+          <p>Total carbohydrates: {foodDetails.nf_total_carbohydrate}g</p>
+          <p>Dietary fiber: {foodDetails.nf_dietary_fiber}g</p>
+          <p>Sugars: {foodDetails.nf_sugars}g</p>
+          <p>Protein: {foodDetails.nf_protein}g</p>
+          <p>Potassium: {foodDetails.nf_potassium}mg</p>
+          <img
+            src={`${foodDetails.photo.thumb}`}
+            alt={`Thumbnail of ${foodDetails.food_name}`}
+          />
+        </div>
+        <button>Delete {foodDetails.name}</button>
+      </>
     );
   };
 
