b5f5688af892b5ba17c1a8453aa562aa62c25e4d
lehelen19 <le.helen19@gmail.com>
2023-06-08 11:29:56 -0700

    Configure theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#374f22',
+
+          secondary: '#99c79b',
+
+          accent: '#c0c18f',
+
+          neutral: '#201f13',
+
+          'base-100': '#f4f3ec',
+
+          info: '#c0c18f',
+
+          success: '#6ee7b7',
+
+          warning: '#fbbf24',
+
+          error: '#ef4444',
+        },
+      },
+    ],
   },
   plugins: [require('daisyui')],
 };
