c57967aa8dcc21fa50a12b704b85ba50a55aa4fc
lehelen19 <le.helen19@gmail.com>
2023-06-09 11:25:25 -0700

    Relayout delete buttons

--- src/pages/FoodItemDetailPage/FoodItemDetailPage.js
+++ src/pages/FoodItemDetailPage/FoodItemDetailPage.js
@@ -15,7 +15,39 @@ const FoodItemDetailPage = ({ user }) => {
   useEffect(() => {
     const fetchFoodDetails = async () => {
       try {
-        const foundFoodDetails = await getFoodDetails(id);
+        // const foundFoodDetails = await getFoodDetails(id);
+        const foundFoodDetails = {
+          name: 'Fat free greek yogurt',
+          quantity: 8,
+          _id: '648361859ad11a8c88955736',
+          createdAt: '2023-06-09T17:29:41.036Z',
+          updatedAt: '2023-06-09T17:29:41.036Z',
+          food_name: 'fat free greek yogurt',
+          brand_name: null,
+          serving_qty: 1,
+          serving_unit: 'oz',
+          serving_weight_grams: 28.35,
+          nf_calories: 23.25,
+          nf_total_fat: 0.04,
+          nf_saturated_fat: 0.03,
+          nf_cholesterol: 1.13,
+          nf_sodium: 9.36,
+          nf_total_carbohydrate: 3.42,
+          nf_dietary_fiber: 0.17,
+          nf_sugars: 3.2,
+          nf_protein: 2.28,
+          nf_potassium: 37.42,
+          nf_p: 32.04,
+          photo: {
+            thumb: 'https://nix-tag-images.s3.amazonaws.com/1814_thumb.jpg',
+            highres: 'https://nix-tag-images.s3.amazonaws.com/1814_highres.jpg',
+            is_user_uploaded: false,
+          },
+          sub_recipe: null,
+          class_code: null,
+          brick_code: null,
+          tag_id: null,
+        };
         setFoodDetails(foundFoodDetails);
       } catch {
         setError('Unable to fetch food details - try again later');
@@ -55,94 +87,99 @@ const FoodItemDetailPage = ({ user }) => {
     return (
       <>
         <div>
-          <h1 className="text-3xl font-bold py-4 capitalize">
+          <h1 className="text-3xl font-bold pt-4 pb-2 capitalize">
             {foodDetails.name}
           </h1>
-          <p>{foodDetails.quantity} currently available</p>
-          <section>
-            <h2 className="text-xl font-semibold">
-              Nutrition Facts for{' '}
-              <span className="capitalize">{foodDetails.food_name}</span>
-            </h2>
-            <hr className="border-1 border-primary mx-4 my-2 w-48" />
-            <table className="table max-w-2xl">
-              <thead></thead>
-              <tbody>
-                <tr>
-                  <th>Serving Size</th>
-                  <td>
-                    {foodDetails.serving_qty} {foodDetails.serving_unit} or{' '}
-                    {foodDetails.serving_weight_grams} grams
-                  </td>
-                </tr>
-                <tr>
-                  <th>Calories</th>
-                  <td>{foodDetails.nf_calories}</td>
-                </tr>
-                <tr>
-                  <th>Total fat</th>
-                  <td>{foodDetails.nf_total_fat}g</td>
-                </tr>
-                <tr>
-                  <th>Saturated fat</th>
-                  <td>{foodDetails.nf_saturated_fat}g</td>
-                </tr>
-                <tr>
-                  <th>Cholesterol</th>
-                  <td>{foodDetails.nf_cholesterol}mg</td>
-                </tr>
-                <tr>
-                  <th>Sodium</th>
-                  <td>{foodDetails.nf_sodium}mg</td>
-                </tr>
-                <tr>
-                  <th>Total carbohydrates</th>
-                  <td>{foodDetails.nf_total_carbohydrate}g</td>
-                </tr>
-                <tr>
-                  <th>Dietary fiber</th>
-                  <td>{foodDetails.nf_dietary_fiber}g</td>
-                </tr>
-                <tr>
-                  <th>Sugars</th>
-                  <td>{foodDetails.nf_sugars}g</td>
-                </tr>
-                <tr>
-                  <th>Protein</th>
-                  <td>{foodDetails.nf_protein}g</td>
-                </tr>
-                <tr>
-                  <th>Potassium</th>
-                  <td>{foodDetails.nf_potassium}mg</td>
-                </tr>
-              </tbody>
-            </table>
-            <div>
-              <img
-                src={`${foodDetails.photo.thumb}`}
-                alt={`Thumbnail of ${foodDetails.food_name}`}
-              />
-            </div>
-          </section>
+          <div>
+            <section>
+              <p>{foodDetails.quantity} currently available</p>
+              <div className="w-24">
+                <img
+                  src={`${foodDetails.photo.thumb}`}
+                  alt={`Thumbnail of ${foodDetails.food_name}`}
+                  className="rounded-md"
+                />
+              </div>
+            </section>
+            <section>
+              <h2 className="text-xl font-semibold">
+                Nutrition Facts for{' '}
+                <span className="capitalize">{foodDetails.food_name}</span>
+              </h2>
+              <hr className="border-1 border-primary mx-4 my-2 w-48" />
+              <table className="table max-w-2xl">
+                <thead></thead>
+                <tbody>
+                  <tr>
+                    <th>Serving Size</th>
+                    <td>
+                      {foodDetails.serving_qty} {foodDetails.serving_unit} or{' '}
+                      {foodDetails.serving_weight_grams} grams
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>Calories</th>
+                    <td>{foodDetails.nf_calories}</td>
+                  </tr>
+                  <tr>
+                    <th>Total fat</th>
+                    <td>{foodDetails.nf_total_fat}g</td>
+                  </tr>
+                  <tr>
+                    <th>Saturated fat</th>
+                    <td>{foodDetails.nf_saturated_fat}g</td>
+                  </tr>
+                  <tr>
+                    <th>Cholesterol</th>
+                    <td>{foodDetails.nf_cholesterol}mg</td>
+                  </tr>
+                  <tr>
+                    <th>Sodium</th>
+                    <td>{foodDetails.nf_sodium}mg</td>
+                  </tr>
+                  <tr>
+                    <th>Total carbohydrates</th>
+                    <td>{foodDetails.nf_total_carbohydrate}g</td>
+                  </tr>
+                  <tr>
+                    <th>Dietary fiber</th>
+                    <td>{foodDetails.nf_dietary_fiber}g</td>
+                  </tr>
+                  <tr>
+                    <th>Sugars</th>
+                    <td>{foodDetails.nf_sugars}g</td>
+                  </tr>
+                  <tr>
+                    <th>Protein</th>
+                    <td>{foodDetails.nf_protein}g</td>
+                  </tr>
+                  <tr>
+                    <th>Potassium</th>
+                    <td>{foodDetails.nf_potassium}mg</td>
+                  </tr>
+                </tbody>
+              </table>
+            </section>
+          </div>
+          {(() => {
+            if (user && confirmDelete) {
+              return (
+                <div>
+                  <p>Are you sure you want to delete {foodDetails.name}?</p>
+                  <button onClick={handleDelete}>Delete</button>
+                  <button onClick={handleCancelDelete}>Cancel</button>
+                  {error && <p>{error}</p>}
+                </div>
+              );
+            } else if (user && !confirmDelete) {
+              return (
+                <button onClick={() => setConfirmDelete(true)}>
+                  Delete {foodDetails.name}
+                </button>
+              );
+            }
+          })()}
         </div>
-        {(() => {
-          if (user && confirmDelete) {
-            return (
-              <>
-                <p>Are you sure you want to delete {foodDetails.name}?</p>
-                <button onClick={handleDelete}>Delete</button>
-                <button onClick={handleCancelDelete}>Cancel</button>
-                {error && <p>{error}</p>}
-              </>
-            );
-          } else if (user && !confirmDelete) {
-            return (
-              <button onClick={() => setConfirmDelete(true)}>
-                Delete {foodDetails.name}
-              </button>
-            );
-          }
-        })()}
       </>
     );
   };
