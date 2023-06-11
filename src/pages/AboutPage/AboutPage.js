b4219bd37b4c53fb7d20fdcefa5e10605a5f490c
lehelen19 <le.helen19@gmail.com>
2023-06-11 15:43:39 -0700

    Style About info

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -1,59 +1,72 @@
 const AboutPage = () => {
   return (
     <main>
-      <h1>About the Yolo Food Bank</h1>
-
-      <div className="w-2/3">
+      <h1 className="text-3xl font-bold text-center mt-4 mb-6">
+        About the Yolo Food Bank
+      </h1>
+      <div className="sm:grid grid-cols-2 gap-5 items-center mx-6">
         <img
           src={process.env.PUBLIC_URL + '/img/yolo.jpg'}
-          alt="A picute of the Yolo Food Bank building"
+          alt="The Yolo Food Bank building"
+          className="max-w-2/3"
         />
+        <div className="flex flex-col items-center text-center">
+          <section className="my-1">
+            <h2 className="font-semibold">Address</h2>
+            <p>
+              <a
+                href="https://goo.gl/maps/ky4S87W7UL3ZW25z5"
+                className="text-blue-600 underline hover:italic"
+              >
+                233 Harter Ave, Woodland, CA 95776
+              </a>
+            </p>
+          </section>
+          <section className="my-1">
+            <h2 className="font-semibold">Hours</h2>
+            <p>M-F: 8:30 AM – 5 PM</p>
+            <p>Closed on Saturdays & Sundays</p>
+          </section>
+          <section className="my-1">
+            <h2 className="font-semibold">Contact us</h2>
+            <ul>
+              <li>(530) 668-0690</li>
+              <li>
+                <a
+                  href="https://yolofoodbank.org/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-blue-600 underline hover:italic"
+                >
+                  Visit our website
+                </a>
+              </li>
+            </ul>
+          </section>
+        </div>
       </div>
-      <section>
-        <h2>Address</h2>
-        <p>233 Harter Ave, Woodland, CA 95776</p>
-      </section>
-      <section>
-        <h2>Hours</h2>
-        <p>M-F: 8:30 AM – 5 PM</p>
-        <p>Closed on Saturdays & Sundays</p>
-      </section>
-      <section>
-        <h2>Contact us</h2>
-        <ul>
-          <li>(530) 668-0690</li>
-          <li>
-            <a
-              href="https://yolofoodbank.org/"
-              target="_blank"
-              rel="noreferrer"
-              className="underline hover:italic"
-            >
-              Website
-            </a>
-          </li>
-        </ul>
-      </section>
-      <p>
-        Per the official website, Yolo Food Bank coordinates the recovery,
-        storage, and distribution of more than 8 million pounds of food
-        annually. We collaborate with a network of grocers and retailers,
-        farmers and distributors, the private sector and governmental agencies,
-        and 84 partner agencies countywide.
-      </p>
-      <p>
-        Our other programs include Eat Well Yolo, Eat Home Yolo, and Kids
-        Farmers Market. Check out more resources and volunteer/advocate on the{' '}
-        <a
-          href="https://yolofoodbank.org/"
-          target="_blank"
-          rel="noreferrer"
-          className="underline hover:italic"
-        >
-          main website
-        </a>
-        .
-      </p>
+      <div className="m-6">
+        <p>
+          Per the official website, Yolo Food Bank coordinates the recovery,
+          storage, and distribution of more than 8 million pounds of food
+          annually. We collaborate with a network of grocers and retailers,
+          farmers and distributors, the private sector and governmental
+          agencies, and 84 partner agencies countywide.
+        </p>
+        <p>
+          Our other programs include Eat Well Yolo, Eat Home Yolo, and Kids
+          Farmers Market. Check out more resources and volunteer/advocate on the{' '}
+          <a
+            href="https://yolofoodbank.org/"
+            target="_blank"
+            rel="noreferrer"
+            className="underline hover:italic"
+          >
+            main website
+          </a>
+          .
+        </p>
+      </div>
     </main>
   );
 };
